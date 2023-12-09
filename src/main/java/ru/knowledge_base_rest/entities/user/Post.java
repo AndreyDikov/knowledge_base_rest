205f184fa49c48f07ebfa2975ecb1ca1baeba410
AndreyDikov <dikovandrey856@gmail.com>
2023-12-09 22:55:27 +0500

    User and Post entities have been created

---
+++ src/main/java/ru/knowledge_base_rest/entities/user/Post.java
@@ -0,0 +1,28 @@
+package ru.knowledge_base_rest.entities.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL)
+    private List<User> users;
+}
---
+++ src/main/java/ru/knowledge_base_rest/entities/user/User.java
@@ -0,0 +1,25 @@
+package ru.knowledge_base_rest.entities.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH,
+            CascadeType.REFRESH, CascadeType.MERGE})
+    @JoinColumn(name = "post_id")
+    private Post postId;
+}
