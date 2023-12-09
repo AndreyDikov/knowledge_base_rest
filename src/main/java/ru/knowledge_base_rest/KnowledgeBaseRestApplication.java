1445bbbf670c5ef73cf98bd5c52b1f6bdb2690fc
AndreyDikov <dikovandrey856@gmail.com>
2023-12-09 18:14:13 +0500

    Initial commit

---
+++ src/main/java/ru/knowledge_base_rest/KnowledgeBaseRestApplication.java
@@ -0,0 +1,13 @@
+package ru.knowledge_base_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBaseRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeBaseRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/knowledge_base_rest/KnowledgeBaseRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.knowledge_base_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBaseRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
